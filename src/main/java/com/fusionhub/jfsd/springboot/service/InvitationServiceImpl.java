7aadb8b681e7ae953527a0ad779af480a23fa564
manishraj27 <manish012raj@gmail.com>
2024-12-08 03:18:37 +0530

    update invitation link

--- src/main/java/com/fusionhub/jfsd/springboot/service/InvitationServiceImpl.java
+++ src/main/java/com/fusionhub/jfsd/springboot/service/InvitationServiceImpl.java
@@ -30,7 +30,7 @@ public class InvitationServiceImpl implements InvitationService {
 		
 		invitationRepository.save(invitation);
 		
-		String invitationLink = "http://localhost:5173/accept_invitation?token="+invitationToken;
+		String invitationLink = "https://fusionhub.netlify.app/accept_invitation?token="+invitationToken;
 		emailService.sendEmailWithToken(email, invitationLink);
 	}
 
